8ef4c3dffda1858385d560142d0571eb6050439e
Travis Taylor <travis@trickfire.com>
2016-11-27 14:58:52 -0700

    Update client to new name; sorry for the change

--- client_example/test.py
+++ client_example/test.py
@@ -10,7 +10,7 @@ import glob
 sys.path.append('gen-py')
 sys.path.append('/usr/lib/python2.7/site-packages/') #wasn't in my path?
 
-from cs5490 import authServe
+from cs5490 import AuthServe
 
 from thrift import Thrift
 from thrift.transport import TSSLSocket
@@ -21,7 +21,7 @@ from thrift.protocol import TBinaryProtocol
 trans = TSSLSocket.TSSLSocket(host='127.0.0.1',port=9090,ca_certs="../ssl/server.crt",validate=False)
 trans_buf = TTransport.TBufferedTransport(trans)
 proto = TBinaryProtocol.TBinaryProtocol(trans_buf)
-client = authServe.Client(proto)
+client = AuthServe.Client(proto)
 
 
 trans.open()
