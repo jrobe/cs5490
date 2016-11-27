ed4b6274019527a280fa20079998fc317bcf0e84
Daniel James <daniel.s.james@icloud.com>
2016-11-27 14:31:34 -0700

    Updated Python example.

--- client_example/test.py
+++ client_example/test.py
@@ -10,7 +10,7 @@ import glob
 sys.path.append('gen-py')
 sys.path.append('/usr/lib/python2.7/site-packages/') #wasn't in my path?
 
-from cs5490 import sanity
+from cs5490 import authServe
 
 from thrift import Thrift
 from thrift.transport import TSSLSocket
@@ -21,9 +21,9 @@ from thrift.protocol import TBinaryProtocol
 trans = TSSLSocket.TSSLSocket(host='127.0.0.1',port=9090,ca_certs="../ssl/server.crt",validate=False)
 trans_buf = TTransport.TBufferedTransport(trans)
 proto = TBinaryProtocol.TBinaryProtocol(trans_buf)
-client = sanity.Client(proto)
+client = authServe.Client(proto)
 
 
 trans.open()
-print client.sanityCheck()
+print client.createAccount("testing", {'email':'testing@test.com'})
 trans.close()
